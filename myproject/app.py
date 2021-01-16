408f4ae99949e99fa15e0c35ba206b09d4ee2305
peterghrong <peter.gh.rong@gmail.com>
2021-01-16 17:01:59 -0500

    update

--- myproject/app.py
+++ myproject/app.py
@@ -1,9 +1,37 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return "hello world"
+    return "home page"
 
-if __name__=="__main__":
+
+@app.route('/about')
+def hello():
+    return "Shopify summer 2021 internship app"
+
+
+@app.route('/search/<string:text>')
+def search_by_text():
+    pass
+
+
+@app.route('/search/<string:characteristic>')
+def search_by_chararcteristics():
+    pass
+
+
+@TODO: this needs to be implemented
+@app.route('/search/<string:image>')
+def search_by_image():
+    pass
+
+
+@app.route('/post')
+def add_image():
+    return "posting test"
+
+
+if __name__ == "__main__":
     app.run()
