718729dced8b4c7804c8109f78b5599d06718c9c
peterghrong <peter.gh.rong@gmail.com>
2021-01-17 12:56:37 -0500

    finished file uploading

--- myproject/app.py
+++ myproject/app.py
@@ -1,37 +1,59 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, flash
+import os
+from werkzeug.utils import secure_filename
+
+# some configurations
 app = Flask(__name__)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config["ALLOWED_EXTENSIONS"] = ALLOWED_EXTENSIONS
+app.config["APP_ROOT"] = APP_ROOT
+UPLOAD_FOLDER = os.path.join(app.config["APP_ROOT"], "static/")
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'anystringthatyoulike'
 
 
-@app.route('/')
-def hello():
-    return "home page"
+# lets display all the images first
+@app.route('/', methods=["GET"])
+def index():
+    # by default flask looks for template folder in the root directory
+    pictures = os.listdir(app.config["UPLOAD_FOLDER"])
+    return render_template("gallery.html", pictures=pictures)
 
 
-@app.route('/about')
-def hello():
-    return "Shopify summer 2021 internship app"
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
 
 
-@app.route('/search/<string:text>')
-def search_by_text():
-    pass
+@app.route('/upload', methods=["POST"])
+def upload():
+    if request.method == 'POST':
+        # if the request is empty then its bad for security
+        if 'pics' not in request.files:
+            flash("Missing file part")
+            return redirect('/')
+
+        files = request.files.getlist("pics")
+
+        for file in files:
+            # check if file names are missing
+            if file.filename == '':
+                flash("Missing file name")
+                return redirect('/')
+
+            if file and allowed_file(file.filename):
+                # secure_file name protects the file structure
+                filename = secure_filename(file.filename)
+
+                # check if the upload folder exists in the file structure
+                if not os.path.isdir(app.config["UPLOAD_FOLDER"]):
+                    os.makedir(app.config["UPLOAD_FOLDER"])
+
+                dest = "/".join([app.config["UPLOAD_FOLDER"], filename])
+                file.save(dest)
+    return redirect('/')
 
 
-@app.route('/search/<string:characteristic>')
-def search_by_chararcteristics():
-    pass
-
-
-@TODO: this needs to be implemented
-@app.route('/search/<string:image>')
-def search_by_image():
-    pass
-
-
-@app.route('/post')
-def add_image():
-    return "posting test"
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
